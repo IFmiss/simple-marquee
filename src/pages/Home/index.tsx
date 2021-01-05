574a3bf7b3a3ad667747313f71cc37e739d86e4a
ifmiss <185098535@qq.com>
2021-01-05 22:58:08 +0800

    update: u

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,19 +6,49 @@ import styles from './home.less'
 
 interface IHomeProps {}
 
+const color = '6f7f81'
+
 const Home: React.FC<IHomeProps> = (props) => {
-  const [stopMarquee, setStopMarquee] = useState(false);
-  const color = Math.random().toString(16).substr(2, 6);
+  const [stopMarquee, setStopMarquee] = useState({
+    stop: true,
+    index: -1
+  });
+
+  const onBtnClick = (index: number) => {
+    setStopMarquee(s => ({
+      stop: index === s.index ? !s.stop : true,
+      index
+    }))
+  }
+
+  const isPlay = (index: number) => {
+    return stopMarquee.index === index && stopMarquee.stop
+  }
 
   return (
     <div className={styles.home}>
       <h1>Simple Marquee for React</h1>
-      <button onClick={() => setStopMarquee(s => !s)}>开关</button>
+      <p>组件使用js实现的 Marquee 功能不适用于多个 Marquee 组件同时展示</p>
+      
+      <code>npm i simple-marquee</code>
+      <p>OR</p>
+      <code>yarn add simple-marquee</code>
+
       <div className={styles.area}>
-        <h3 className={styles.title}>横向</h3>
+        <h3 className={styles.title}>横向文字</h3>
+        <Marquee className={styles.marquee_group}
+          direction={'horizontal'}
+          stop={!(stopMarquee.stop && stopMarquee.index === -1)}>
+          2020年结束了，2021年开始了，为新的一年，加油 ！！！！⛽️&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </Marquee>
+      </div>
+      <button onClick={() => onBtnClick(-1)}>{ isPlay(0) ? '启动' : '关闭' }</button>
+
+      <div className={styles.area}>
+        <h3 className={styles.title}>横向列表</h3>
         <Marquee className={styles.marquee_group}
         direction={'horizontal'}
-        stop={stopMarquee}>
+        stop={!(stopMarquee.stop && stopMarquee.index === 0)}>
           <List style={{
             backgroundColor: `#${color}`
           }}>白日依山尽</List>
@@ -33,162 +63,128 @@ const Home: React.FC<IHomeProps> = (props) => {
           }}>更上一层楼</List>
           <List style={{
             backgroundColor: `#${color}`
-          }}>你好</List>
+          }}>春眠不觉晓</List>
           <List style={{
             backgroundColor: `#${color}`
-          }}>你好</List>
+          }}>处处闻啼鸟</List>
           <List style={{
             backgroundColor: `#${color}`
-          }}>你好</List>
+          }}>夜来风雨声</List>
           <List style={{
             backgroundColor: `#${color}`
-          }}>你好</List>
-          <List style={{
-            backgroundColor: `#${color}`
-          }}>你好</List>
+          }}>花落知多少</List>
         </Marquee>
       </div>
+      <button onClick={() => onBtnClick(0)}>{ isPlay(0) ? '启动' : '关闭' }</button>
 
       <div className={styles.area}>
         <h3 className={styles.title}>Marquee组</h3>
         <div className={styles.marquee_group}>
-          <MarqueeGroup stop={stopMarquee}>
+          <MarqueeGroup stop={!(stopMarquee.stop && stopMarquee.index === 1)}>
             <Marquee
             style={{
               marginBottom: '12px'
             }}
             speed={1.8}
-            direction={'horizontal'}
-            stop={stopMarquee}>
+            indent={10}
+            direction={'horizontal'}>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>枯藤老树昏鸦</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>小桥流水人家</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>古道西风瘦马</List>
             </Marquee>
             <Marquee
             style={{
               marginBottom: '12px'
             }}
-            direction={'horizontal'}
-            speed={1.2}
-            stop={stopMarquee}>
+            direction={'horizontal-reverse'}
+            indent={25}
+            speed={1.2}>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>夕阳西下</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>断肠人在天涯</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>离离原上草</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-            </Marquee>
-            <Marquee
-            style={{
-              marginBottom: '12px'
-            }}
-            direction={'horizontal'}
-            speed={1.4}
-            stop={stopMarquee}>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>一岁一枯荣</List>
             </Marquee>
             <Marquee
             direction={'horizontal'}
-            speed={1.6}
-            stop={stopMarquee}>
+            indent={55}
+            speed={1.4}>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>一岁一枯荣</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>野火烧不尽</List>
               <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
-              <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>春风吹又生</List>
             </Marquee>
           </MarqueeGroup>
         </div>
+
+        <button onClick={() => onBtnClick(1)}>{ isPlay(1) ? '启动' : '关闭' }</button>
       </div>
 
       <div className={styles.area}>
         <h3 className={styles.title}>横向-反方向</h3>
         <Marquee className={styles.marquee_group}
-        direction={'horizontal-reverse'}>
+        direction={'horizontal-reverse'}
+        stop={!(stopMarquee.stop && stopMarquee.index === 2)}>
           <List style={{
                 backgroundColor: `#${color}`
-              }}>你好</List>
+              }}>明天会更好，打工人</List>
         </Marquee>
+
+        <button onClick={() => onBtnClick(2)}>{ isPlay(2) ? '启动' : '关闭' }</button>
       </div>
 
       <div className={styles.area}>
         <h3 className={styles.title}>竖向</h3>
         <Marquee className={styles.marquee_group_vertical}
-        direction={'vertical'}>
+        direction={'vertical'}
+        stop={!(stopMarquee.stop && stopMarquee.index === 3)}>
           <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
+                // backgroundColor: `#${color}`
+              }}>hello</List>
+          <List style={{
+                // backgroundColor: `#${color}`
+              }}>this is a react component</List>
+          <List style={{
+                // backgroundColor: `#${color}`
+              }}>react simple marquee</List>
         </Marquee>
+        <button onClick={() => onBtnClick(3)}>{ isPlay(3) ? '启动' : '关闭' }</button>
       </div>
 
       <div className={styles.area}>
-        <h3 className={styles.title}>竖向</h3>
+        <h3 className={styles.title}>竖向-反方向</h3>
         <Marquee className={styles.marquee_group_vertical}
-        direction={'vertical-reverse'}>
+        direction={'vertical-reverse'}
+        stop={!(stopMarquee.stop && stopMarquee.index === 4)}>
           <List style={{
-                backgroundColor: `#${color}`
-              }}>你好</List>
+                // backgroundColor: `#${color}`
+              }}>11111111111</List>
+          <List style={{
+                // backgroundColor: `#${color}`
+              }}>22222222</List>
+          <List style={{
+                // backgroundColor: `#${color}`
+              }}>33333333</List>
         </Marquee>
+        <button onClick={() => onBtnClick(4)}>{ isPlay(4) ? '启动' : '关闭' }</button>
       </div>
     </div>
   )
