9c68c855f33353f987792189ca6e7db963e95a0b
ifmiss <185098535@qq.com>
2022-10-14 22:29:18 +0800

    update: to 0.0.4

--- lib/cjs/Marquee.d.ts
+++ lib/cjs/Marquee.d.ts
@@ -4,6 +4,7 @@ export interface MarqueeProps extends React.HTMLAttributes<HTMLDivElement> {
     indent?: number;
     direction?: 'horizontal' | 'vertical' | 'horizontal-reverse' | 'vertical-reverse';
     stop?: boolean;
+    pauseOnHover?: boolean;
 }
 declare const _default: React.NamedExoticComponent<MarqueeProps>;
 export default _default;
